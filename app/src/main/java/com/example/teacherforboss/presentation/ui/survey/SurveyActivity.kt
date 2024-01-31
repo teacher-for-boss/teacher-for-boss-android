753a6e0dfe7654026476e9911b517217a84cd752
JIEUNI <jieundaeun@naver.com>
2024-01-31 19:59:52 +0900

    [feat] 프래그먼트 연결 및 전체 설문조사 공통 뷰 구현

--- app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyActivity.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyActivity.kt
@@ -1,4 +1,118 @@
 package com.example.teacherforboss.presentation.ui.survey
 
-class SurveyActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.flowWithLifecycle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.teacherforboss.R
+import com.example.teacherforboss.databinding.ActivitySurveyBinding
+import com.example.teacherforboss.presentation.ui.survey.question.SurveyCompleteFragment
+import com.example.teacherforboss.presentation.ui.survey.question.SurveyJobFragment
+import com.example.teacherforboss.presentation.ui.survey.question.SurveyProblemFragment
+import com.example.teacherforboss.presentation.ui.survey.question.SurveyProblemReasonFragment
+import com.example.teacherforboss.presentation.ui.survey.question.SurveyStudyFragment
+import com.example.teacherforboss.util.base.BindingActivity
+import kotlinx.coroutines.flow.onEach
+
+class SurveyActivity :
+    BindingActivity<ActivitySurveyBinding>(R.layout.activity_survey) {
+    private val surveyViewModel: SurveyViewModel by viewModels()
+    private lateinit var fragmentList: ArrayList<Fragment>
+    private lateinit var onBackPressed: OnBackPressedCallback
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.viewModel = surveyViewModel
+
+        initLayout()
+        addListeners()
+        collectData()
+        setFragmentStateAdapter()
+    }
+
+    private fun initLayout() {
+        with(binding.progressbarSurvey) {
+            min = DEFAULT_PROGRESSBAR
+            max = fragmentList.size.toFloat()
+        }
+    }
+
+    private fun addListeners() {
+        binding.ivSurveyBackBtn.setOnClickListener {
+            navigateToPreviousFragment()
+        }
+
+        binding.btnSurveyNext.setOnClickListener {
+            when (binding.vpSurvey.currentItem) {
+                fragmentList.size - 1 -> {
+                    // TODO 서버통신
+                }
+
+                else -> binding.vpSurvey.currentItem++
+            }
+        }
+    }
+
+    private fun collectData() {
+        surveyViewModel.currentPage.flowWithLifecycle(lifecycle).onEach { currentPage ->
+            binding.progressbarSurvey.progress = currentPage.toFloat() + DEFAULT_PROGRESSBAR
+            when(currentPage) {
+                fragmentList.size - 1 -> {
+                    binding.btnSurveyNext.text = getString(R.string.survey_button_start)
+                }
+                fragmentList.size - 2 -> {
+                    binding.btnSurveyNext.text = getString(R.string.survey_button_complete)
+                }
+                else -> {
+                    binding.btnSurveyNext.text = getString(R.string.survey_button_next)
+                }
+            }
+        }
+    }
+
+    private fun setFragmentStateAdapter() {
+        fragmentList.apply {
+            add(SurveyJobFragment())
+            add(SurveyStudyFragment())
+            add(SurveyProblemFragment())
+            add(SurveyProblemReasonFragment())
+            add(SurveyCompleteFragment())
+        }
+
+        val adapter = SurveyFragmentStateAdapter(fragmentList, this)
+        with(binding.vpSurvey) {
+            this.adapter = adapter
+            isUserInputEnabled = false
+            registerOnPageChangeCallback(object :
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    surveyViewModel.setCurrentPage(position)
+                }
+            })
+        }
+    }
+
+    private fun navigateToPreviousFragment() {
+        when (binding.vpSurvey.currentItem) {
+            FIRST_FRAGMENT -> navigatoToSurveyStart()
+            else -> binding.vpSurvey.currentItem--
+        }
+    }
+
+    private fun navigatoToSurveyStart() {
+        Intent(this, SurveyStartActivity::class.java).apply {
+            startActivity(this)
+            finish()
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_PROGRESSBAR = 1f
+        private const val FIRST_FRAGMENT = 0
+    }
 }
--- app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyViewModel.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyViewModel.kt
@@ -1,4 +1,49 @@
 package com.example.teacherforboss.presentation.ui.survey
 
-class SurveyViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SurveyViewModel : ViewModel() {
+    // val SurveyBtnEnabled: StateFlow<Boolean> = true
+
+    private val _currentPage = MutableStateFlow(FIRST_FRAGMENT_POSITION)
+    val currentPage get() = _currentPage.asStateFlow()
+
+    private val _selectedJob = MutableStateFlow(DEFAULT_SELECTED_NUMBER)
+    val selectedJob get() = _selectedJob.asStateFlow()
+
+    private val _selectedStudy = MutableStateFlow(DEFAULT_SELECTED_NUMBER)
+    val selectedStudy get() = _selectedStudy.asStateFlow()
+
+    private val _selectedProblem = MutableStateFlow(DEFAULT_SELECTED_NUMBER)
+    val selectedProblem get() = _selectedProblem.asStateFlow()
+
+    private val _problemDescription = MutableStateFlow("")
+    val problemDescription get() = _problemDescription.asStateFlow()
+
+    fun setCurrentPage(position: Int) {
+        _currentPage.value = position
+    }
+
+    fun setSelectedJob(answer: Int) {
+        _selectedJob.value = answer
+    }
+
+    fun setSelectedStudy(answer: Int) {
+        _selectedStudy.value = answer
+    }
+
+    fun setSelectedProblem(answer: Int) {
+        _selectedProblem.value = answer
+    }
+
+    fun setProblemDescription(answer: String) {
+        _problemDescription.value = answer
+    }
+
+    companion object {
+        private const val FIRST_FRAGMENT_POSITION = 0
+        private const val DEFAULT_SELECTED_NUMBER = 0
+    }
 }

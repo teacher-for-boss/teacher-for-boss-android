b4cf814563c5baf43b8fdcce933efc17718b807f
JIEUNI <jieundaeun@naver.com>
2024-01-31 21:51:12 +0900

    [feat] 선택 유무에 따른 버튼 활성화/비활성화 로직 구현

---
+++ app/src/main/java/com/example/teacherforboss/presentation/type/SurveyType.kt
@@ -0,0 +1,11 @@
+package com.example.teacherforboss.presentation.type
+
+enum class SurveyType(
+    val position: Int
+) {
+    JOB(0),
+    STUDY(1),
+    PROBLEM(2),
+    DESCRIPTION(3),
+    COMPLETE(4)
+}
--- app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyViewModel.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.teacherforboss.presentation.ui.survey
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.teacherforboss.presentation.type.SurveyType
+import com.example.teacherforboss.util.combineAll
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class SurveyViewModel : ViewModel() {
-    // val SurveyBtnEnabled: StateFlow<Boolean> = true
-
     private val _currentPage = MutableStateFlow(FIRST_FRAGMENT_POSITION)
     val currentPage get() = _currentPage.asStateFlow()
 
@@ -21,6 +26,27 @@ class SurveyViewModel : ViewModel() {
 
     private val _problemDescription = MutableStateFlow("")
     val problemDescription get() = _problemDescription.asStateFlow()
+
+    val surveyBtnEnabled: StateFlow<Boolean> = listOf(
+        currentPage,
+        selectedJob,
+        selectedStudy,
+        selectedProblem,
+        problemDescription,
+    ).combineAll()
+        .map { values ->
+            val currentPage = values[0] as Int
+            val selectedJob = values[1] as Int
+            val selectedStudy = values[2] as Int
+            val selectedProblem = values[3] as Int
+            val problemDescription = values[4] as String
+
+            (currentPage == SurveyType.JOB.position && selectedJob != DEFAULT_SELECTED_NUMBER && selectedJob != null) ||
+                (currentPage == SurveyType.STUDY.position && selectedStudy != DEFAULT_SELECTED_NUMBER && selectedJob != null) ||
+                (currentPage == SurveyType.PROBLEM.position && selectedProblem != DEFAULT_SELECTED_NUMBER && selectedJob != null) ||
+                (currentPage == SurveyType.DESCRIPTION.position && problemDescription.isNotBlank()) ||
+                (currentPage == SurveyType.COMPLETE.position)
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), false)
 
     fun setCurrentPage(position: Int) {
         _currentPage.value = position

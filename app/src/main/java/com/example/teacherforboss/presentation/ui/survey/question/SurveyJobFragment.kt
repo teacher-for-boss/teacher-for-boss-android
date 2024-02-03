3a140fb6b3e2ed1ab1e4915c0f11b207d0588541
JIEUNI <jieundaeun@naver.com>
2024-02-04 06:34:42 +0900

    [refactor] SurveyJobFragment 로직 깔끔하게 수정

--- app/src/main/java/com/example/teacherforboss/presentation/ui/survey/question/SurveyJobFragment.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/survey/question/SurveyJobFragment.kt
@@ -1,8 +1,9 @@
 package com.example.teacherforboss.presentation.ui.survey.question
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.RadioButton
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import com.example.teacherforboss.R
 import com.example.teacherforboss.databinding.FragmentSurveyJobBinding
@@ -24,54 +25,32 @@ class SurveyJobFragment :
     private fun addListeners() {
         binding.rgSurveyJob.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
-                R.id.btn_survey_job_boss -> {
-                    binding.btnSurveyJobBoss.isChecked = true
-                    viewModel.setSelectedJob(1)
-                }
-
-                R.id.btn_survey_job_pre_boss -> {
-                    binding.btnSurveyJobPreBoss.isChecked = true
-                    viewModel.setSelectedJob(2)
-                }
-
-                R.id.btn_survey_job_employee -> {
-                    binding.btnSurveyJobEmployee.isChecked = true
-                    viewModel.setSelectedJob(3)
-                }
-
-                R.id.btn_survey_job_official -> {
-                    binding.btnSurveyJobOfficial.isChecked = true
-                    viewModel.setSelectedJob(4)
-                }
-
-                R.id.btn_survey_job_public -> {
-                    binding.btnSurveyJobPublic.isChecked = true
-                    viewModel.setSelectedJob(5)
-                }
+                R.id.btn_survey_job_boss -> setRadioCheckedJob(binding.btnSurveyJobBoss, 1)
+                R.id.btn_survey_job_pre_boss -> setRadioCheckedJob(binding.btnSurveyJobPreBoss, 2)
+                R.id.btn_survey_job_employee -> setRadioCheckedJob(binding.btnSurveyJobEmployee, 3)
+                R.id.btn_survey_job_official -> setRadioCheckedJob(binding.btnSurveyJobOfficial, 4)
+                R.id.btn_survey_job_public -> setRadioCheckedJob(binding.btnSurveyJobPublic, 5)
             }
         }
 
         with(binding) {
-            tvSurveyJobBoss.setOnClickListener {
-                binding.btnSurveyJobBoss.isChecked = true
-                viewModel.setSelectedJob(1)
-            }
-            tvSurveyJobPreBoss.setOnClickListener {
-                binding.btnSurveyJobPreBoss.isChecked = true
-                viewModel.setSelectedJob(2)
-            }
-            tvSurveyJobEmployee.setOnClickListener {
-                binding.btnSurveyJobEmployee.isChecked = true
-                viewModel.setSelectedJob(3)
-            }
-            tvSurveyJobOfficial.setOnClickListener {
-                binding.btnSurveyJobOfficial.isChecked = true
-                viewModel.setSelectedJob(4)
-            }
-            tvSurveyJobPublic.setOnClickListener {
-                binding.btnSurveyJobPublic.isChecked = true
-                viewModel.setSelectedJob(5)
-            }
+            setCheckedJob(tvSurveyJobBoss, btnSurveyJobBoss, 1)
+            setCheckedJob(tvSurveyJobPreBoss, btnSurveyJobPreBoss, 2)
+            setCheckedJob(tvSurveyJobEmployee, btnSurveyJobEmployee, 3)
+            setCheckedJob(tvSurveyJobOfficial, btnSurveyJobOfficial, 4)
+            setCheckedJob(tvSurveyJobPublic, btnSurveyJobPublic, 5)
         }
+    }
+
+    private fun setCheckedJob(textView: TextView, radioButton: RadioButton, answerId: Int) {
+        textView.setOnClickListener {
+            radioButton.isChecked = true
+            viewModel.setSelectedJob(answerId)
+        }
+    }
+
+    private fun setRadioCheckedJob(radioButton: RadioButton, answerId: Int) {
+        radioButton.isChecked = true
+        viewModel.setSelectedJob(answerId)
     }
 }

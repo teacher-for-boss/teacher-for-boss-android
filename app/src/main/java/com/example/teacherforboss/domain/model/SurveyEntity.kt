677c9126de6b119f0147b81d7f1446f55402daff
JIEUNI <jieundaeun@naver.com>
2024-02-05 20:38:09 +0900

    [add] survey 관련 Entity 추가(mapper 구현)

--- app/src/main/java/com/example/teacherforboss/domain/model/SurveyEntity.kt
+++ app/src/main/java/com/example/teacherforboss/domain/model/SurveyEntity.kt
@@ -1,6 +1,8 @@
 package com.example.teacherforboss.domain.model
 
 data class SurveyEntity(
-    val question1: Long,
-    val question2: Long
+    val question1: Int,
+    val question2: ArrayList<Int>,
+    val question3: Int,
+    val question4: String
 )

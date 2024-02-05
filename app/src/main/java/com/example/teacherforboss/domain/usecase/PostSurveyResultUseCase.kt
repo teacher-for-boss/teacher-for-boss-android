996e76cbe820aefee032135078d4ead025dbd54c
JIEUNI <jieundaeun@naver.com>
2024-02-05 20:35:50 +0900

    [delete] API 명세서에 따른 이름 변경을 위한 파일 삭제

--- app/src/main/java/com/example/teacherforboss/domain/usecase/PostSurveyResultUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.teacherforboss.domain.usecase
-
-import com.example.teacherforboss.domain.repository.SurveyRepository
-
-class PostSurveyResultUseCase (
-    private val surveyRepository: SurveyRepository
-){
-}

5fb17f0915bbdf872b6c3900906dd12e0118165a
younheejae <hjy10045@sookmyung.ac.kr>
2024-02-17 13:46:29 +0900

    feat: 메인홈 화면

--- app/src/main/java/com/example/teacherforboss/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/main/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.example.teacherforboss.presentation.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.teacherforboss.ui.theme.TeacherForBossTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.teacherforboss.R
+import com.example.teacherforboss.databinding.ActivityMainBinding
+import com.example.teacherforboss.presentation.ui.main.menu.HomeFragment
+import com.example.teacherforboss.presentation.ui.main.menu.MyClassFragment
+import com.example.teacherforboss.presentation.ui.main.menu.SchoolFragment
+import com.example.teacherforboss.presentation.ui.main.menu.TeacherFragment
+import com.example.teacherforboss.presentation.ui.main.menu.TestFragment
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setBottomNavigationView()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout, HomeFragment()).commit()
+        }
+    }
+
+    fun setBottomNavigationView() {
+        binding.mainBottomNavigationView.setOnItemSelectedListener {item->
+            when(item.itemId) {
+                R.id.menu_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout, HomeFragment()).commit()
+                    true }
+                R.id.menu_school -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout,SchoolFragment()).commit()
+                    true }
+                R.id.menu_test -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout,TestFragment()).commit()
+                    true }
+                R.id.menu_teacher -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout,TeacherFragment()).commit()
+                    true }
+                R.id.menu_my_class -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout,MyClassFragment()).commit()
+                    true }
+                else -> false
+            }
+        }
     }
 }

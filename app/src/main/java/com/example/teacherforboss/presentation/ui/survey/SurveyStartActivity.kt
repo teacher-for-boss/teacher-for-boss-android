5e928a96d6167640b9e46c0200bb3a22ff4a201c
JIEUNI <jieundaeun@naver.com>
2024-02-17 22:22:52 +0900

    [feat] 설문조사 시작 뷰 : 안드로이드 이전 버튼 클릭 시 MainActivity로 이동

--- app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyStartActivity.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/survey/SurveyStartActivity.kt
@@ -2,18 +2,23 @@ package com.example.teacherforboss.presentation.ui.survey
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import com.example.teacherforboss.R
 import com.example.teacherforboss.databinding.ActivitySurveyStartBinding
 import com.example.teacherforboss.presentation.ui.auth.login.LoginActivity
+import com.example.teacherforboss.presentation.ui.main.MainActivity
 import com.example.teacherforboss.util.base.BindingActivity
 
 class SurveyStartActivity :
     BindingActivity<ActivitySurveyStartBinding>(R.layout.activity_survey_start) {
+    private lateinit var onBackPressed: OnBackPressedCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initLayout()
         addListeners()
+        onBackPressedBtn()
     }
 
     private fun initLayout() {
@@ -23,7 +28,7 @@ class SurveyStartActivity :
 
     private fun addListeners() {
         binding.layoutSurveyStartBtn.setOnClickListener { navigateToSurvey() }
-        binding.ivSurveyStartBackBtn.setOnClickListener { navigateToLogin() }
+        binding.ivSurveyStartBackBtn.setOnClickListener { navigateToMain() }
     }
 
     private fun navigateToSurvey() {
@@ -33,10 +38,18 @@ class SurveyStartActivity :
         }
     }
 
-    private fun navigateToLogin() {
-        Intent(this, LoginActivity::class.java).apply {
+    private fun navigateToMain() {
+        Intent(this, MainActivity::class.java).apply {
             startActivity(this)
             finish()
+        }
+    }
+
+    private fun onBackPressedBtn() {
+        onBackPressed = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                navigateToMain()
+            }
         }
     }
 }

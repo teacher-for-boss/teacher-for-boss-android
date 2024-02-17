8849d5d0d1ad181782f893fdafec38941b98f3b4
oh-bom <spdlqj888888@gmail.com>
2024-02-17 17:24:43 +0900

    feat:exam result test img

--- app/src/main/java/com/example/teacherforboss/presentation/ui/examResult/examResultActivity.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/examResult/examResultActivity.kt
@@ -2,6 +2,9 @@ package com.example.teacherforboss.presentation.ui.examResult
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import com.example.teacherforboss.R
@@ -18,32 +21,47 @@ class examResultActivity : AppCompatActivity() {
     val fragmentManager: FragmentManager =supportFragmentManager
 
     lateinit var binding:ActivityExamResultBinding
+    lateinit var behavior: BottomSheetBehavior<LinearLayout>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityExamResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val viewPager=binding.viewPager2
-//        val tabLayout=binding.tabLayout
-//
-//        val fragmentList= listOf(
-//            WrongNotesFragment(), AnalysisFragment(), ScoreFragment()
-//        )
-//
-//        viewPager.adapter=examResultViewPagerAdapter(fragmentList =fragmentList,this)
-//        TabLayoutMediator(tabLayout,viewPager){tab,position->
-//            if(position==0) tab.text="오답노트"
-//            else if(position==1) tab.text="결과 분석"
-//            else tab.text="등수 확인"
-//        }.attach()
+        initBottomSheet()
 
-        //bottom sheet dialog
-//        val bottomsheetView=layoutInflater.inflate(R.layout.fragment_result_bottom_sheet,null)
-//        val bottomSheetDialog=BottomSheetDialog(this)
-//        bottomSheetDialog.setContentView(bottomsheetView)
-//
-//        bottomSheetDialog.behavior.state=BottomSheetBehavior.STATE_HALF_EXPANDED
-//        bottomSheetDialog.show()
+        val viewPager=binding.viewPager2
+        val tabLayout=binding.tabLayout
+
+        val fragmentList= listOf(
+            WrongNotesFragment(), AnalysisFragment(), ScoreFragment()
+        )
+
+        viewPager.adapter=examResultViewPagerAdapter(fragmentList =fragmentList,this)
+        TabLayoutMediator(tabLayout,viewPager){tab,position->
+            if(position==0) tab.text="오답노트"
+            else if(position==1) tab.text="결과 분석"
+            else tab.text="등수 확인"
+        }.attach()
 
     }
+
+    private fun initBottomSheet(){
+        behavior=BottomSheetBehavior.from(binding.bottomSheet)
+        behavior.state=BottomSheetBehavior.STATE_EXPANDED
+        behavior.isDraggable=true
+        behavior.isHideable=false
+
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // 슬라이드 되는 도중 계속 호출
+                // called continuously while dragging
+                Log.d("bottom", "onStateChanged: 드래그 중")
+            }
+        })
+    }
+
 }

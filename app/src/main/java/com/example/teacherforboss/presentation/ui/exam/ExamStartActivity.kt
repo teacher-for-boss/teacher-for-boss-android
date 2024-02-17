caf430c2f6d2f1403ec4384c5b44077db4118f0f
JIEUNI <jieundaeun@naver.com>
2024-02-17 23:12:46 +0900

    [feat] #28 시험보기 시작 뷰 : 안드로이드 기본 이전 버튼 구현

--- app/src/main/java/com/example/teacherforboss/presentation/ui/exam/ExamStartActivity.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/exam/ExamStartActivity.kt
@@ -3,16 +3,20 @@ package com.example.teacherforboss.presentation.ui.exam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import com.example.teacherforboss.R
 import com.example.teacherforboss.databinding.ActivityExamStartBinding
 import com.example.teacherforboss.presentation.ui.main.MainActivity
 import com.example.teacherforboss.util.base.BindingActivity
 
 class ExamStartActivity : BindingActivity<ActivityExamStartBinding>(R.layout.activity_exam_start) {
+    private lateinit var onBackPressed: OnBackPressedCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         addListeners()
+        onBackPressedBtn()
     }
 
     private fun addListeners(){
@@ -32,5 +36,14 @@ class ExamStartActivity : BindingActivity<ActivityExamStartBinding>(R.layout.act
             startActivity(this)
             finish()
         }
+    }
+
+    private fun onBackPressedBtn() {
+        onBackPressed = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                navigateToHome()
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, onBackPressed)
     }
 }

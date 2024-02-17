5a875f35b64dea541f410fc8b1028f89e7479e48
JIEUNI <jieundaeun@naver.com>
2024-02-17 23:24:10 +0900

    [feat] #28 시험보기 시작 뷰 : 서비스에 Spannable 구현

--- app/src/main/java/com/example/teacherforboss/presentation/ui/exam/ExamStartActivity.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/exam/ExamStartActivity.kt
@@ -3,7 +3,12 @@ package com.example.teacherforboss.presentation.ui.exam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
 import com.example.teacherforboss.R
 import com.example.teacherforboss.databinding.ActivityExamStartBinding
 import com.example.teacherforboss.presentation.ui.main.MainActivity
@@ -15,8 +20,27 @@ class ExamStartActivity : BindingActivity<ActivityExamStartBinding>(R.layout.act
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initLayout()
         addListeners()
         onBackPressedBtn()
+    }
+
+    private fun initLayout() {
+        binding.examStartWelcomeName.text = SpannableString(
+            getString(R.string.exam_start_title)
+        ).apply {
+            setSpan(
+                ForegroundColorSpan(
+                    ContextCompat.getColor(
+                        this@ExamStartActivity,
+                        R.color.main_purple_00
+                    )
+                ),
+                SERVICE_START,
+                SERVICE_END,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
     }
 
     private fun addListeners(){
@@ -45,5 +69,10 @@ class ExamStartActivity : BindingActivity<ActivityExamStartBinding>(R.layout.act
             }
         }
         onBackPressedDispatcher.addCallback(this, onBackPressed)
+    }
+
+    companion object {
+        const val SERVICE_START = 4
+        const val SERVICE_END = 7
     }
 }

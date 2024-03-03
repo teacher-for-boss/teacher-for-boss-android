015c1bf26df08a2f17d6a232bfcc6aa003f9957d
oh-bom <spdlqj888888@gmail.com>
2024-03-03 17:00:29 +0900

    chore

--- app/src/main/java/com/example/teacherforboss/data/api/AuthApiClient.kt
+++ app/src/main/java/com/example/teacherforboss/data/api/AuthApiClient.kt
@@ -6,37 +6,72 @@ import com.example.teacherforboss.GlobalApplication
 import com.example.teacherforboss.data.intercepter.AuthAuthenticator
 import com.example.teacherforboss.data.intercepter.AuthInterceptor
 import com.example.teacherforboss.data.tokenmanager.TokenManager
+import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.PrimitiveIterator
+import javax.inject.Inject
 
-object AuthApiClient {
-    private val tokenManager: TokenManager = TokenManager
-    var mHttpLoggingInterceptor= HttpLoggingInterceptor()
-        .setLevel(HttpLoggingInterceptor.Level.BODY)
+//사용 x
+//class AuthApiClient @Inject constructor(
+//    @ApplicationContext val context: Context,
+//    private var retrofit: Retrofit,
+//
+//    ) {
+//    private val tokenManager: TokenManager = TokenManager
+//    private val mHttpLoggingInterceptor = HttpLoggingInterceptor()
+//        .setLevel(HttpLoggingInterceptor.Level.BODY)
+//
+//    private val mOkHttpClient = OkHttpClient.Builder()
+//        .addInterceptor(mHttpLoggingInterceptor)
+//        .addInterceptor(AuthInterceptor(context,tokenManager))
+//        .authenticator(AuthAuthenticator(context,tokenManager))
+//        .build()
+//
+////    private var mRetrofit: Retrofit? = null
+//
+//    val client: Retrofit
+//        get() {
+//            if (retrofit == null) {
+//                retrofit = Retrofit.Builder()
+//                    .baseUrl(BuildConfig.BASE_URL)
+//                    .client(mOkHttpClient)
+//                    .addConverterFactory(GsonConverterFactory.create())
+//                    .build()
+//            }
+//            return retrofit
+//        }
+//}
 
-    var mOkHttpClient= OkHttpClient.Builder()
-        .addInterceptor(mHttpLoggingInterceptor)
-        .addInterceptor(AuthInterceptor(getAppContenxt()))
-        .authenticator(AuthAuthenticator(TokenManager, getAppContenxt()))
-        .build()
 
-    var mRetrofit: Retrofit?=null
-    val client: Retrofit?
-        get(){
-            if(mRetrofit ==null){
-                mRetrofit = Retrofit.Builder()
-                    .baseUrl(BuildConfig.BASE_URL)
-                    .client(mOkHttpClient)
-                    .addConverterFactory(GsonConverterFactory.create())//gson:google에서 만든 java용 json
-                    .build()
-            }
-            return mRetrofit
-        }
-}
-
-private fun getAppContenxt(): Context {
-    return GlobalApplication.instance.applicationContext
-}
+//object AuthApiClient {
+//    private val tokenManager: TokenManager = TokenManager
+//    var mHttpLoggingInterceptor= HttpLoggingInterceptor()
+//        .setLevel(HttpLoggingInterceptor.Level.BODY)
+//
+//    var mOkHttpClient= OkHttpClient.Builder()
+//        .addInterceptor(mHttpLoggingInterceptor)
+//        .addInterceptor(AuthInterceptor(getAppContenxt()))
+//        .authenticator(AuthAuthenticator(TokenManager, getAppContenxt()))
+//        .build()
+//
+//    var mRetrofit: Retrofit?=null
+//    val client: Retrofit?
+//        get(){
+//            if(mRetrofit ==null){
+//                mRetrofit = Retrofit.Builder()
+//                    .baseUrl(BuildConfig.BASE_URL)
+//                    .client(mOkHttpClient)
+//                    .addConverterFactory(GsonConverterFactory.create())//gson:google에서 만든 java용 json
+//                    .build()
+//            }
+//            return mRetrofit
+//        }
+//}
+//
+//private fun getAppContenxt(): Context {
+//    return GlobalApplication.instance.applicationContext
+//}
 

2c746dff7f982da1b137f1408ec2d8028f0a5654
oh-bom <spdlqj888888@gmail.com>
2024-03-04 15:04:20 +0900

    feat: imgAdapter, toast확장함수 추가

--- app/src/main/java/com/example/teacherforboss/util/context/ContextExt.kt
+++ app/src/main/java/com/example/teacherforboss/util/context/ContextExt.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.annotation.StringRes
+import java.time.Duration
 
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -12,3 +14,7 @@ fun Context.hideKeyboard(view: View) {
 }
 
 fun Context.stringOf(@StringRes resId: Int) = getString(resId)
+
+fun Context.showToast(msg:String,duration: Int= Toast.LENGTH_LONG){
+    Toast.makeText(this,msg,duration).show()
+}

f58a6f7ffb5888769fa4226ac95477fe04048c5c
oh-bom <spdlqj888888@gmail.com>
2024-03-06 20:47:13 +0900

    feat:오답노트 리스트 리사이클러 뷰 연결

--- app/src/main/java/com/example/teacherforboss/presentation/ui/examResult/screens/WrongNotesFragment.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/examResult/screens/WrongNotesFragment.kt
@@ -1,13 +1,16 @@
 package com.example.teacherforboss.presentation.ui.examResult.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.teacherforboss.databinding.FragmentResultWrongNotesBinding
+import com.example.teacherforboss.domain.model.exams.ExamResultWrongNotesEntity
 import com.example.teacherforboss.presentation.ui.examResult.examResultActivity
 import com.example.teacherforboss.presentation.ui.examResult.examResultViewModel
 import com.example.teacherforboss.presentation.ui.examResult.adapter.rv_adapter_wrong_notes
@@ -17,6 +20,7 @@ import com.example.teacherforboss.presentation.ui.examResult.testDto.wrongNotesD
 class WrongNotesFragment : Fragment() {
     private val viewModel by activityViewModels<examResultViewModel>()
     lateinit var binding:FragmentResultWrongNotesBinding
+    private lateinit var rv_notes:List<ExamResultWrongNotesEntity.WrongQuestionEntity>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +35,19 @@ class WrongNotesFragment : Fragment() {
 //        binding=DataBindingUtil.inflate(layoutInflater,R.layout.fragment_result_wrong_notes,container,false)
 //        binding.lifecycleOwner=this
 
-        val notes=viewModel.dummy_wrongnotes
+
+        //api 결과 수신
+        viewModel.examResultWrongNotesLiveData.observe(viewLifecycleOwner, Observer { result->
+            rv_notes=result.examWrongQuestionList
+        })
 
         val activity=activity as examResultActivity
         binding.rvList.layoutManager=LinearLayoutManager(activity.applicationContext,LinearLayoutManager.VERTICAL,false)
-        binding.rvList.adapter= rv_adapter_wrong_notes(notes)
+        binding.rvList.adapter=rv_adapter_wrong_notes(rv_notes)
 
-        // Inflate the layout for this fragment
+        //val notes=viewModel.dummy_wrongnotes
+//        binding.rvList.adapter= rv_adapter_wrong_notes(notes)
+
         return binding.root
     }
 

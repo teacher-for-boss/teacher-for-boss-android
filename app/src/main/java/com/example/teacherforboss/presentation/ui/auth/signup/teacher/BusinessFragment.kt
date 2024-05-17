ec5e1480a3b9ace1d8b97ac2365a962e1a060a59
oh-bom <spdlqj888888@gmail.com>
2024-05-17 09:04:39 +0900

    feat:사업자 인증 뷰(달력x)

---
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/auth/signup/teacher/BusinessFragment.kt
@@ -0,0 +1,55 @@
+package com.example.teacherforboss.presentation.ui.auth.signup.teacher
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import com.example.teacherforboss.R
+import com.example.teacherforboss.databinding.FragmentBusinessBinding
+import com.example.teacherforboss.presentation.ui.auth.signup.SignupActivity
+import com.example.teacherforboss.presentation.ui.auth.signup.SignupViewModel
+import java.util.Calendar
+
+class BusinessFragment : Fragment() {
+    private lateinit var binding:FragmentBusinessBinding
+    private val viewModel by activityViewModels<SignupViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_business,container, false)
+        val activity=activity as SignupActivity
+
+        addListeners()
+
+        return binding.root
+    }
+
+    private fun addListeners(){
+        binding.calendar.setOnClickListener {
+            val cal=Calendar.getInstance()
+            val data=DatePickerDialog.OnDateSetListener{view,year,month,day->
+               viewModel._openDate.value="${year} / ${month} / ${day}"
+            }
+            context?.let { it1 ->
+                DatePickerDialog(
+                    it1,data,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
+        }
+
+    }
+
+
+
+
+}

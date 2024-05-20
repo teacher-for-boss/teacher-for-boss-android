886aecce55088890cd5887a0a25c94ca84d0a389
cyyoon0311 <cy_0311@naver.com>
2024-05-21 03:04:40 +0900

    design:프로필 사진 다이얼로그 제작

--- app/src/main/java/com/example/teacherforboss/presentation/ui/auth/signup/boss/BossProfileFragment.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/auth/signup/boss/BossProfileFragment.kt
@@ -1,24 +1,20 @@
 package com.example.teacherforboss.presentation.ui.auth.signup.boss
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
+import android.app.AlertDialog
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.example.teacherforboss.R
 import com.example.teacherforboss.data.model.response.BaseResponse
 import com.example.teacherforboss.databinding.FragmentBossProfileBinding
-import com.example.teacherforboss.databinding.FragmentSignupStartBinding
 import com.example.teacherforboss.presentation.ui.auth.signup.SignupActivity
 import com.example.teacherforboss.presentation.ui.auth.signup.SignupViewModel
+import com.example.teacherforboss.presentation.ui.auth.signup.basic.PasswordFragment
 
 class BossProfileFragment : Fragment() {
 
@@ -38,12 +34,20 @@ class BossProfileFragment : Fragment() {
         val activity=activity as SignupActivity
         val nicknameBox = binding.nicknameBox
 
+        viewModel._isEmailVerified_str.value="F"
+        viewModel._isEmailVerified.value=false
+
+
+        binding.profileImage.setOnClickListener(){
+            showDialog()
+        }
+
+
 
         binding.nicknameVerifyBtn.setOnClickListener(){
             val emailRegex = Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
             viewModel.email_check.value=emailRegex.matches(viewModel.liveEmail.value.toString())
             viewModel.emailUser()
-            nicknameBox.setBackgroundResource(R.drawable.selector_signup_error)
 
         }
 
@@ -53,21 +57,82 @@ class BossProfileFragment : Fragment() {
                 is BaseResponse.Success->{
 
                     viewModel.emailAuthId.value=it.data?.result?.emailAuthId!!//result로 전달받은 emailAuthId 저장
-
+                    nicknameBox.setBackgroundResource(R.drawable.selector_signup_success)
                 }
                 is BaseResponse.Error->{
-                    nicknameBox.setBackgroundResource(R.drawable.selector_signup_error)
+                    if(it.msg=="이미 가입된 이메일입니다.") {
+                        nicknameBox.setBackgroundResource(R.drawable.selector_signup_error)
+                    }
                 }
                 else -> {}
             }
         }
-        nicknameBox.addTextChangedListener(object : TextWatcher{
+        var emailCode=binding.nicknameBox.text.toString()
+        viewModel.emailCheckUser(emailCode)
+        viewModel.emailCheckResult.observe(viewLifecycleOwner){
+            when(it){
+                is BaseResponse.Loading->{
+                }
+                is BaseResponse.Success->{
+
+
+                    //viewModel.setEmailVerifiedStatus(it.data?.isSuccess!!&&it.data?.result?.checked!!)
+                    if(it.data?.isSuccess!!&&it.data?.result?.checked!!){
+                        viewModel._isEmailVerified_str.value="T"
+                        viewModel._isEmailVerified.value=true
+
+
+                        binding.nextBtn.setOnClickListener {
+                            Log.d("email",viewModel.email.value!!)
+                            activity.gotoNextFragment(PasswordFragment())
+                        }
+
+                        viewModel.stopTimer()
+                    }
+
+
+
+                    //후에 회원가입 버튼 클릭시 현재입력되어있는(liveEmail,viewModel email)값이 confirmed map에 있는지 확인 후
+                    //없다면 이메일 인증을 다시 받아야 하니 그거에 맞는 안내 알림 설정!
+                    //ex) 사용자가 a라는 이메일로 인증받고 b라는 이메일을 다시 입력했을 경우 방지를 위함
+//                    var tempEmailMap= mutableMapOf<String, LiveData<Boolean>>()
+//                    tempEmailMap[email]=viewModel.isEmailVerified
+//                    viewModel.confirmedEmail.postValue(tempEmailMap)
+
+                }
+                is BaseResponse.Error->{
+
+                    binding.nextBtn.isEnabled = false
+
+                    viewModel._isEmailVerified_str.value="F"
+                    viewModel._isEmailVerified.value=false
+
+                }
+
+                else -> {}
+            }
+        }
+
+
+        /*nicknameBox.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {nicknameBox.setBackgroundResource(R.drawable.selector_signup)}
-        })
+        })*/
 
         return binding.root
 
     }
+    private fun showDialog(){
+        val builder = AlertDialog.Builder(requireContext())
+
+        val inflater = LayoutInflater.from(requireContext())
+        val dialogView = inflater.inflate(R.layout.dialog_profile_image, null)
+
+        builder.setView(dialogView)
+
+        val dialog = builder.create()
+
+        dialog.show()    }
+
 }

ecfefd6d9ed57fc50768e49c60e176a0a3cb706b
oh-bom <spdlqj888888@gmail.com>
2024-06-09 15:27:21 +0900

    Revert "fix: 티쳐로 회원가입 request 수정"
    This reverts commit bf89a1f76fc5b0e8fa4037bf829196e93f1d4432.

--- app/src/main/java/com/example/teacherforboss/data/model/request/login/SocialLoginRequest.kt
+++ app/src/main/java/com/example/teacherforboss/data/model/request/login/SocialLoginRequest.kt
@@ -7,15 +7,15 @@ import java.util.Date
 data class SocialLoginRequest(
     @SerializedName("email")
     var email:String,
-//    @SerializedName("name")
-//    var name:String,
-//    @SerializedName("phone")
-//    var phone:String,
-//    @SerializedName("gender")
-//    var gender:Int?,
-//    @SerializedName("birthDate")
-//    var birthDate: String?,
-//    @SerializedName("profileImg")
-//    var profileImg:String?
+    @SerializedName("name")
+    var name:String,
+    @SerializedName("phone")
+    var phone:String,
+    @SerializedName("gender")
+    var gender:Int?,
+    @SerializedName("birthDate")
+    var birthDate: String?,
+    @SerializedName("profileImg")
+    var profileImg:String?
 
 )

88b94439feb59f8cf8b1f00350aaec8b44c110fb
a1rhun <83606547+a1rhun@users.noreply.github.com>
2024-06-12 01:31:40 +0900

    feat/스티키 헤더 임시 완료. 가로 리사이클러뷰 완성.

--- app/src/main/java/com/example/teacherforboss/presentation/ui/teachertalkmain/basic/TeacherTalkMainFragment.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/teachertalkmain/basic/TeacherTalkMainFragment.kt
@@ -3,10 +3,12 @@ package com.example.teacherforboss.presentation.ui.teachertalkmain.basic
 
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.teacherforboss.R
 import com.example.teacherforboss.databinding.FragmentTeacherTalkMainBinding
@@ -28,12 +30,35 @@ class TeacherTalkMainFragment :
 
         val teacherTalkCategoryAdapter = TeacherTalkCategoryAdpapter(requireContext())
         binding.rvTeacherTalkCategory.adapter = teacherTalkCategoryAdapter
-        binding.rvTeacherTalkCategory.addItemDecoration(HorizontalSpaceItemDecoration(17))
         teacherTalkCategoryAdapter.setTeacherTalkCategoryList(viewModel.mockTeacherTalkCategoryList)
 
+        //dropdown
         val items = resources.getStringArray(R.array.dropdown_items)
         val adapter = CustomAdapter(requireContext(), items)
         binding.spinnerDropdown.adapter = adapter
+
+        //scrollview
+        binding.svTeacherTalkMain.run {
+            header = binding.teacherTalkWidget2
+            stickListener = { _ ->
+                Log.d("LOGGER_TAG", "stickListener")
+            }
+            freeListener = { _ ->
+                Log.d("LOGGER_TAG", "freeListener")
+            }
+        }
+
+        binding.rvTeacherTalkCard.layoutManager = LinearLayoutManager(requireContext())
+
+//        // RecyclerView를 담은 위젯 높이를 동적으로 설정
+//        binding.svTeacherTalkMain.viewTreeObserver.addOnGlobalLayoutListener {
+//            val parentHeight = binding.svTeacherTalkMain.height
+//            val otherViewsHeight = binding.teacherTalkWidget1.height + binding.teacherTalkWidget2.height + binding.teacherTalkWidget3.height
+//            val widget4Height = parentHeight - otherViewsHeight
+//            binding.teacherTalkWidget4.layoutParams.height = widget4Height
+//            binding.teacherTalkWidget4.requestLayout()
+//        }
+
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {

938c2aea6c29eaa3d01f8e9b61178fb75a3ee688
JIEUNI <jieundaeun@naver.com>
2024-07-07 01:36:53 +0900

    [feat] #40 마이페이지 뷰 - UI 구현

--- app/src/main/java/com/example/teacherforboss/presentation/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/mypage/MyPageFragment.kt
@@ -1,60 +1,24 @@
 package com.example.teacherforboss.presentation.ui.mypage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.teacherforboss.R
+import com.example.teacherforboss.databinding.FragmentMyPageBinding
+import com.example.teacherforboss.util.base.BindingFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun initLayout() {
+        if (true) { // 보스인 경우 -> 티처를 디폴트로 하고 보스를 뷰 수정하는 것이 좋을듯
+            with(binding) {
+                tvMyPageLevel.visibility = View.GONE
+                layoutMyPageLevelInfo.visibility = View.GONE
+                // menu bar setImage
+                // menu bar String 수정
             }
+        }
     }
 }

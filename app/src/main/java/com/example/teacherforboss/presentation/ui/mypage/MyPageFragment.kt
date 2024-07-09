2b6468571383e29f62129aa15c165dbef1bbc87e
JIEUNI <jieundaeun@naver.com>
2024-07-09 12:35:59 +0900

    [feat] #40 마이페이지 뷰 - 로그아웃 시 Dialog 연결

--- app/src/main/java/com/example/teacherforboss/presentation/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/mypage/MyPageFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.example.teacherforboss.R
 import com.example.teacherforboss.databinding.FragmentMyPageBinding
 import com.example.teacherforboss.util.base.BindingFragment
+import com.example.teacherforboss.util.component.DialogPopupFragment
 import com.example.teacherforboss.util.context.navigateToWebView
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
@@ -29,7 +30,20 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
         with(binding) {
             includeMyPageMenuInquire.root.setOnClickListener { requireActivity().startActivity(requireActivity().navigateToWebView(INQUIRE_WEB_LINK)) }
             includeMyPageMenuTerms.root.setOnClickListener { requireActivity().startActivity(requireActivity().navigateToWebView(TERMS_WEB_LINK)) }
+            tvLogOutBtn.setOnClickListener { showLogoutDialogFragment() }
         }
+    }
+
+    private fun showLogoutDialogFragment() {
+        // TODO clickRightBtn에 로그아웃 뷰모델 로직 추가
+        DialogPopupFragment(
+            title = getString(R.string.dialog_logout_title),
+            content = getString(R.string.dialog_logout_content),
+            leftBtnText = getString(R.string.dialog_exit),
+            rightBtnText = getString(R.string.dialog_logout_btn),
+            clickLeftBtn = {},
+            clickRightBtn = {},
+        ).show(parentFragmentManager, LOGOUT_DIALOG)
     }
 
     companion object {
@@ -37,5 +51,6 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
             "https://docs.google.com/forms/d/e/1FAIpQLScvoVxh-1jlqyKhVKiFS4pZDhk-GtYbZOHKh4KJHveutN2TYw/viewform"
         private const val TERMS_WEB_LINK =
             "https://beautiful-pharaoh-385.notion.site/3f2236a9632b4edca4b7a0175308f43b?pvs=4"
+        private const val LOGOUT_DIALOG = "logoutModal"
     }
 }

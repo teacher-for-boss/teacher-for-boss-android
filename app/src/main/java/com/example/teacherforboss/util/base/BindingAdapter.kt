6b900cb6c285e13da6dd35ee088f4d3aada458c4
JIEUNI <jieundaeun@naver.com>
2024-07-11 02:19:42 +0900

    [feat] #40 마이페이지 뷰 - 티처 레벨 Type 구현

---
+++ app/src/main/java/com/example/teacherforboss/util/base/BindingAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.teacherforboss.util.base
+
+import android.content.res.ColorStateList
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("image")
+fun setImageResource(imageView: ImageView, resId: Int) {
+    val drawable = ContextCompat.getDrawable(imageView.context, resId)
+    imageView.setImageDrawable(drawable)
+}
+
+@BindingAdapter("textColor")
+fun setTextColor(textView: TextView, resId: Int) {
+    val colorRes = ContextCompat.getColor(textView.context, resId)
+    textView.setTextColor(colorRes)
+}
+
+@BindingAdapter("backgroundTint")
+fun setBackGroundTint(textView: TextView, resId: Int) {
+    val colorRes = ContextCompat.getColor(textView.context, resId)
+    textView.backgroundTintList = ColorStateList.valueOf(colorRes)
+}

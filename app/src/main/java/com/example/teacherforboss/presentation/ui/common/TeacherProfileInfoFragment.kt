1aacfb116c1ddf8e5f56f06b6a9ea92cea5af45e
JIEUNI <jieundaeun@naver.com>
2024-07-17 01:24:04 +0900

    [feat] #67 티처 상세 프로필 - 티처 정보 Fragment UI 구현

---
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/common/TeacherProfileInfoFragment.kt
@@ -0,0 +1,14 @@
+package com.example.teacherforboss.presentation.ui.common
+
+import android.os.Bundle
+import android.view.View
+import com.example.teacherforboss.R
+import com.example.teacherforboss.databinding.FragmentTeacherProfileInfoBinding
+import com.example.teacherforboss.util.base.BindingFragment
+
+
+class TeacherProfileInfoFragment : BindingFragment<FragmentTeacherProfileInfoBinding>(R.layout.fragment_teacher_profile_info) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/teacherforboss/presentation/ui/common/TeacherProfileRecentAnswerFragment.kt
@@ -0,0 +1,14 @@
+package com.example.teacherforboss.presentation.ui.common
+
+import android.os.Bundle
+import android.view.View
+import com.example.teacherforboss.R
+import com.example.teacherforboss.databinding.FragmentTeacherProfileRecentAnswerBinding
+import com.example.teacherforboss.util.base.BindingFragment
+
+class TeacherProfileRecentAnswerFragment :
+    BindingFragment<FragmentTeacherProfileRecentAnswerBinding>(R.layout.fragment_teacher_profile_recent_answer) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

e1f7ea946acf2b4d4d3121bb9bec005ab35fb4ed
younheejae <hjy10045@sookmyung.ac.kr>
2024-09-02 20:14:57 +0900

    [fix] #120 QA 수정 - ui 및 이미지불러오기

--- app/src/main/java/com/company/teacherforboss/presentation/ui/mypage/community/rvAdapterMyPageQuestion.kt
+++ app/src/main/java/com/company/teacherforboss/presentation/ui/mypage/community/rvAdapterMyPageQuestion.kt
@@ -6,11 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.company.teacherforboss.databinding.RvItemMyPageQuestionCardBinding
 import com.company.teacherforboss.domain.model.mypage.MyPageQuestionEntity
 import com.company.teacherforboss.presentation.ui.community.teacher_talk.body.TeacherTalkBodyActivity
 import com.company.teacherforboss.util.base.LocalDateFormatter
+import com.company.teacherforboss.util.view.loadCircularImage
 
 class rvAdapterMyPageQuestion(
     private val context: Context,
@@ -29,9 +29,7 @@ class rvAdapterMyPageQuestion(
                     widgetCardViewStatementSolved.visibility = View.VISIBLE
                     widgetCardViewStatementNotSolved.visibility = View.GONE
 
-                    Glide.with(root.context)
-                        .load(question.selectedTeacher)
-                        .into(ivSelectedTeacher)
+                    question.selectedTeacher?.let { ivSelectedTeacher.loadCircularImage(it) }
                 }
                 else {
                     widgetCardViewStatementNotSolved.visibility = View.VISIBLE
@@ -39,10 +37,10 @@ class rvAdapterMyPageQuestion(
                 }
 
                 root.setOnClickListener {
-                    val intent = Intent(context, TeacherTalkBodyActivity::class.java).apply {
+                    Intent(context, TeacherTalkBodyActivity::class.java).apply {
                         putExtra("questionId", question.questionId)
+                        context.startActivity(this)
                     }
-                    context.startActivity(intent)
                 }
             }
         }

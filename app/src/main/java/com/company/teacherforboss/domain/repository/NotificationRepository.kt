a7445488885c8f0cb056fa96ee507561733a81f4
younheejae <hjy10045@sookmyung.ac.kr>
2024-11-03 05:04:39 +0900

    [fix] #204 개별 알림 읽음 처리

--- app/src/main/java/com/company/teacherforboss/domain/repository/NotificationRepository.kt
+++ app/src/main/java/com/company/teacherforboss/domain/repository/NotificationRepository.kt
@@ -1,13 +1,12 @@
 package com.company.teacherforboss.domain.repository
 
 import com.company.teacherforboss.domain.model.notification.NotificationListEntity
-import com.company.teacherforboss.domain.model.notification.NotificationReadEntity
 import com.company.teacherforboss.domain.model.notification.NotificationSettingEntity
 
 interface NotificationRepository {
     suspend fun getNotifications(lastNotificationId: Long):Result<NotificationListEntity>
 
-    suspend fun readNotification(notificatioinId:Long):Result<NotificationReadEntity>
+    suspend fun readNotification(notificationId:Long):Result<Unit>
 
     suspend fun getNotificationSetting(): Result<NotificationSettingEntity>
 
